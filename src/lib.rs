05eff3d661209fea7327f21522112dc5a3bd37f8
sidit77 <sidit77@gmail.com>
2021-05-30 22:43:26 +0200

    swaped back to webgl

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,62 @@
-use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use std::cell::RefCell;
 use std::rc::Rc;
-
-fn window() -> web_sys::Window {
-    web_sys::window().expect("no global `window` exists")
-}
+use web_sys::{WebGlRenderingContext, WebGlShader, WebGlProgram};
 
 fn request_animation_frame(f: &Closure<dyn FnMut()>) {
-    window()
+    web_sys::window().unwrap()
         .request_animation_frame(f.as_ref().unchecked_ref())
         .expect("should register `requestAnimationFrame` OK");
 }
 
-fn document() -> web_sys::Document {
-    window()
-        .document()
-        .expect("should have a document on window")
-}
-
-fn canvas() -> web_sys::HtmlCanvasElement {
-    document()
-        .get_element_by_id("canvas")
-        .expect("should have a element called 'canvas'")
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .expect("'canvas' should be a canvas")
-}
-
-fn context(canvas: &web_sys::HtmlCanvasElement) -> web_sys::CanvasRenderingContext2d {
-    canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap()
-}
-
 #[wasm_bindgen(start)]
-pub fn start() {
+pub fn start() -> Result<(), JsValue>{
 
-    let window = window();
-    let canvas = canvas();
-    let context = context(&canvas);
+    let window = web_sys::window().unwrap();
+    let canvas = window.document().unwrap().get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let context = canvas.get_context("webgl").unwrap().unwrap().dyn_into::<WebGlRenderingContext>()?;
+
+    let vert_shader = compile_shader(
+        &context,
+        WebGlRenderingContext::VERTEX_SHADER,
+        r#"
+        attribute vec4 position;
+        void main() {
+            gl_Position = position;
+        }
+    "#,
+    )?;
+    let frag_shader = compile_shader(
+        &context,
+        WebGlRenderingContext::FRAGMENT_SHADER,
+        r#"
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+    "#,
+    )?;
+    let program = link_program(&context, &vert_shader, &frag_shader)?;
+    context.use_program(Some(&program));
+
+    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+
+    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
+    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+    unsafe {
+        let vert_array = js_sys::Float32Array::view(&vertices);
+
+        context.buffer_data_with_array_buffer_view(
+            WebGlRenderingContext::ARRAY_BUFFER,
+            &vert_array,
+            WebGlRenderingContext::STATIC_DRAW,
+        );
+    }
+
+    context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+    context.enable_vertex_attrib_array(0);
+
 
     let f = Rc::new(RefCell::new(None));
     let g = f.clone();
@@ -52,38 +65,69 @@ pub fn start() {
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
         canvas.set_width(window.inner_width().unwrap().as_f64().unwrap() as u32 - 4);
         canvas.set_height(window.inner_height().unwrap().as_f64().unwrap() as u32 - 4);
-        context.clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+        context.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
         i += 0.1;
 
-        context.begin_path();
 
-        // Draw the outer circle.
-        //context.move_to(i, i);
-        context
-            .arc(75.0 + i, 75.0 + i, 50.0, 0.0, f64::consts::PI * 2.0)
-            .unwrap();
+        context.clear_color(0.5 + 0.5 * f32::sin(0.2 * i), 0.0, 0.0, 1.0);
+        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
 
-        // Draw the mouth.
-        context.move_to(110.0 + i, 75.0 + i);
-        context.arc(75.0 + i, 75.0 + i, 35.0, 0.0, f64::consts::PI).unwrap();
-
-        // Draw the left eye.
-        context.move_to(65.0 + i, 65.0 + i);
-        context
-            .arc(60.0 + i, 65.0 + i, 5.0, 0.0, f64::consts::PI * 2.0)
-            .unwrap();
-
-        // Draw the right eye.
-        context.move_to(95.0 + i, 65.0 + i);
-        context
-            .arc(90.0 + i, 65.0 + i, 5.0, 0.0, f64::consts::PI * 2.0)
-            .unwrap();
-
-        context.stroke();
+        context.draw_arrays(WebGlRenderingContext::TRIANGLES, 0, (vertices.len() / 3) as i32);
 
         request_animation_frame(f.borrow().as_ref().unwrap());
     }) as Box<dyn FnMut()>));
 
     request_animation_frame(g.borrow().as_ref().unwrap());
 
+    Ok(())
+}
+
+pub fn compile_shader(
+    context: &WebGlRenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+pub fn link_program(
+    context: &WebGlRenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
 }
