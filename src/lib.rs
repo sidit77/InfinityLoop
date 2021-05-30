caaaa112e2edfc6df5cf1fe4c819d4b942bff58b
sidit77 <sidit77@gmail.com>
2021-05-30 17:22:19 +0200

    resizing canvas and animationsRequests

--- src/lib.rs
+++ src/lib.rs
@@ -1,123 +1,89 @@
+use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+fn canvas() -> web_sys::HtmlCanvasElement {
+    document()
+        .get_element_by_id("canvas")
+        .expect("should have a element called 'canvas'")
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .expect("'canvas' should be a canvas")
+}
+
+fn context(canvas: &web_sys::HtmlCanvasElement) -> web_sys::CanvasRenderingContext2d {
+    canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap()
+}
 
 #[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+pub fn start() {
 
-    let context = canvas
-        .get_context("webgl")?
-        .unwrap()
-        .dyn_into::<WebGlRenderingContext>()?;
+    let window = window();
+    let canvas = canvas();
+    let context = context(&canvas);
 
-    let vert_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::VERTEX_SHADER,
-        r#"
-        attribute vec4 position;
-        void main() {
-            gl_Position = position;
-        }
-    "#,
-    )?;
-    let frag_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::FRAGMENT_SHADER,
-        r#"
-        void main() {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
-    "#,
-    )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
 
-    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+    let mut i = 0.0;
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        canvas.set_width(window.inner_width().unwrap().as_f64().unwrap() as u32 - 4);
+        canvas.set_height(window.inner_height().unwrap().as_f64().unwrap() as u32 - 4);
+        context.clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+        i += 0.1;
 
-    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+        context.begin_path();
 
-    // Note that `Float32Array::view` is somewhat dangerous (hence the
-    // `unsafe`!). This is creating a raw view into our module's
-    // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-    // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-    // causing the `Float32Array` to be invalid.
-    //
-    // As a result, after `Float32Array::view` we have to be very careful not to
-    // do any memory allocations before it's dropped.
-    unsafe {
-        let vert_array = js_sys::Float32Array::view(&vertices);
+        // Draw the outer circle.
+        //context.move_to(i, i);
+        context
+            .arc(75.0 + i, 75.0 + i, 50.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
 
-        context.buffer_data_with_array_buffer_view(
-            WebGlRenderingContext::ARRAY_BUFFER,
-            &vert_array,
-            WebGlRenderingContext::STATIC_DRAW,
-        );
-    }
+        // Draw the mouth.
+        context.move_to(110.0 + i, 75.0 + i);
+        context.arc(75.0 + i, 75.0 + i, 35.0, 0.0, f64::consts::PI).unwrap();
 
-    context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
-    context.enable_vertex_attrib_array(0);
+        // Draw the left eye.
+        context.move_to(65.0 + i, 65.0 + i);
+        context
+            .arc(60.0 + i, 65.0 + i, 5.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
 
-    context.clear_color(0.3, 0.3, 0.3, 1.0);
-    context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+        // Draw the right eye.
+        context.move_to(95.0 + i, 65.0 + i);
+        context
+            .arc(90.0 + i, 65.0 + i, 5.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
 
-    context.draw_arrays(
-        WebGlRenderingContext::TRIANGLES,
-        0,
-        (vertices.len() / 3) as i32,
-    );
-    Ok(())
-}
+        context.stroke();
 
-pub fn compile_shader(
-    context: &WebGlRenderingContext,
-    shader_type: u32,
-    source: &str,
-) -> Result<WebGlShader, String> {
-    let shader = context
-        .create_shader(shader_type)
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
 
-    if context
-        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(shader)
-    } else {
-        Err(context
-            .get_shader_info_log(&shader)
-            .unwrap_or_else(|| String::from("Unknown error creating shader")))
-    }
-}
+    request_animation_frame(g.borrow().as_ref().unwrap());
 
-pub fn link_program(
-    context: &WebGlRenderingContext,
-    vert_shader: &WebGlShader,
-    frag_shader: &WebGlShader,
-) -> Result<WebGlProgram, String> {
-    let program = context
-        .create_program()
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
-
-    if context
-        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(program)
-    } else {
-        Err(context
-            .get_program_info_log(&program)
-            .unwrap_or_else(|| String::from("Unknown error creating program object")))
-    }
 }
