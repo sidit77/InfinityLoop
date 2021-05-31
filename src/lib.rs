78e35d167177dd42cbe6f7740af74239dd679661
sidit77 <sidit77@gmail.com>
2021-05-31 23:16:42 +0200

    click handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use std::cell::RefCell;
+use std::cell::{RefCell, Cell};
 use std::rc::Rc;
 
-use glam::{Mat4, Vec3};
+use glam::{Mat4, Vec3, Vec2, Quat};
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
 use web_sys::WebGl2RenderingContext;
@@ -29,6 +29,16 @@ pub fn start() -> Result<(), JsValue>{
     let window = web_sys::window().unwrap();
     let canvas = window.document().unwrap().get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
     let gl = canvas.get_context("webgl2").unwrap().unwrap().dyn_into::<WebGl2RenderingContext>()?;
+
+    let last_click = Rc::new(Cell::new(Vec2::new(0.0,0.0)));
+    {
+        let last_click = last_click.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            last_click.set(Vec2::new(event.client_x() as f32, event.client_y() as f32));
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
 
     let program = compile_program(&gl, &[
         (WebGl2RenderingContext::VERTEX_SHADER, include_str!("shader/vertex.glsl")),
@@ -89,7 +99,13 @@ pub fn start() -> Result<(), JsValue>{
         gl.clear_color(0.5 + 0.5 * f32::sin(0.2 * i), 0.0, 0.0, 1.0);
         gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
-        let obj_mat = Mat4::from_rotation_z(0.04 * i);
+        let point = last_click.get();
+        let point = Vec2::new(point.x / canvas.width() as f32, point.y / canvas.height() as f32);
+        let point = Vec2::new(2.0 * point.x - 1.0, 2.0 * (1.0 - point.y) - 1.0);
+        let point = camera.to_matrix().inverse().transform_point3(point.extend(0.0));
+        let obj_mat = Mat4::from_rotation_translation(
+            Quat::from_rotation_z(0.04 * i),
+            point);
         gl.uniform_matrix4fv_with_f32_array(Some(&obj_location), false, &obj_mat.to_cols_array());
 
         gl.draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, (vertices.len() / 2) as i32);
