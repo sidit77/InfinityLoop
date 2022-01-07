5fe4437481a9f665b3090deecd98c44be70f2d3e
sidit77 <sidit77@gmail.com>
2022-01-07 23:05:53 +0100

    fixed main to render generated shapes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 mod meshes;
 
+use glam::Vec2;
 use miniquad::*;
-
-#[repr(C)]
-struct Vec2 {
-    x: f32,
-    y: f32,
-}
-#[repr(C)]
-struct Vertex {
-    pos: Vec2,
-    uv: Vec2,
-}
 
 struct Stage {
     pipeline: Pipeline,
@@ -20,31 +10,15 @@ struct Stage {
 
 impl Stage {
     pub fn new(ctx: &mut Context) -> Stage {
-        #[rustfmt::skip]
-            let vertices: [Vertex; 4] = [
-            Vertex { pos : Vec2 { x: -0.5, y: -0.5 }, uv: Vec2 { x: 0., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y: -0.5 }, uv: Vec2 { x: 1., y: 0. } },
-            Vertex { pos : Vec2 { x:  0.5, y:  0.5 }, uv: Vec2 { x: 1., y: 1. } },
-            Vertex { pos : Vec2 { x: -0.5, y:  0.5 }, uv: Vec2 { x: 0., y: 1. } },
-        ];
-        let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, &vertices);
 
-        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
-        let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, &indices);
+        let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, meshes::VERTICES);
 
-        let pixels: [u8; 4 * 4 * 4] = [
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-            0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-        ];
-        let texture = Texture::from_rgba8(ctx, 4, 4, &pixels);
+        let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, meshes::INDICES);
 
         let bindings = Bindings {
             vertex_buffers: vec![vertex_buffer],
-            index_buffer: index_buffer,
-            images: vec![texture],
+            index_buffer,
+            images: Vec::new(),
         };
 
         let shader = Shader::new(ctx, shader::VERTEX, shader::FRAGMENT, shader::meta()).unwrap();
@@ -52,10 +26,7 @@ impl Stage {
         let pipeline = Pipeline::new(
             ctx,
             &[BufferLayout::default()],
-            &[
-                VertexAttribute::new("pos", VertexFormat::Float2),
-                VertexAttribute::new("uv", VertexFormat::Float2),
-            ],
+            &[VertexAttribute::new("pos", VertexFormat::Float2)],
             shader,
         );
 
@@ -73,14 +44,12 @@ impl EventHandler for Stage {
 
         ctx.apply_pipeline(&self.pipeline);
         ctx.apply_bindings(&self.bindings);
-        for i in 0..10 {
-            let t = t + i as f64 * 0.3;
 
-            ctx.apply_uniforms(&shader::Uniforms {
-                offset: (t.sin() as f32 * 0.5, (t * 3.).cos() as f32 * 0.5),
-            });
-            ctx.draw(0, 6, 1);
-        }
+        ctx.apply_uniforms(&shader::Uniforms {
+            offset: (t.sin() as f32 * 0.5, (t * 3.).cos() as f32 * 0.5),
+        });
+        let model = meshes::MODEL2;
+        ctx.draw(model.start, model.len() as i32, 1);
         ctx.end_render_pass();
 
         ctx.commit_frame();
@@ -98,24 +67,20 @@ mod shader {
 
     pub const VERTEX: &str = r#"#version 100
     attribute vec2 pos;
-    attribute vec2 uv;
     uniform vec2 offset;
-    varying lowp vec2 texcoord;
     void main() {
-        gl_Position = vec4(pos + offset, 0, 1);
-        texcoord = uv;
+        gl_Position = vec4(pos * 0.5 + offset, 0, 1);
     }"#;
 
     pub const FRAGMENT: &str = r#"#version 100
-    varying lowp vec2 texcoord;
-    uniform sampler2D tex;
+
     void main() {
-        gl_FragColor = texture2D(tex, texcoord);
+        gl_FragColor = vec4(1,0,0,1);
     }"#;
 
     pub fn meta() -> ShaderMeta {
         ShaderMeta {
-            images: vec!["tex".to_string()],
+            images: vec![],
             uniforms: UniformBlockLayout {
                 uniforms: vec![UniformDesc::new("offset", UniformType::Float2)],
             },
