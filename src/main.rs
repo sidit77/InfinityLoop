342652c50c4c98c0fad1bbcd0c4993e3e158fbaf
sidit77 <sidit77@gmail.com>
2022-01-07 23:21:20 +0100

    attempt to transfer shaders

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 mod meshes;
 
-use glam::Vec2;
+use glam::{Mat4, Vec2};
 use miniquad::*;
 
-struct Stage {
+struct Game {
     pipeline: Pipeline,
     bindings: Bindings,
 }
 
-impl Stage {
-    pub fn new(ctx: &mut Context) -> Stage {
+impl Game {
+    pub fn new(ctx: &mut Context) -> Game {
 
         let vertex_buffer = Buffer::immutable(ctx, BufferType::VertexBuffer, meshes::VERTICES);
-
         let index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, meshes::INDICES);
 
         let bindings = Bindings {
@@ -30,11 +29,11 @@ impl Stage {
             shader,
         );
 
-        Stage { pipeline, bindings }
+        Game { pipeline, bindings }
     }
 }
 
-impl EventHandler for Stage {
+impl EventHandler for Game {
     fn update(&mut self, _ctx: &mut Context) {}
 
     fn draw(&mut self, ctx: &mut Context) {
@@ -46,7 +45,11 @@ impl EventHandler for Stage {
         ctx.apply_bindings(&self.bindings);
 
         ctx.apply_uniforms(&shader::Uniforms {
-            offset: (t.sin() as f32 * 0.5, (t * 3.).cos() as f32 * 0.5),
+            camera: Mat4::IDENTITY,
+            model: Mat4::IDENTITY,
+            color: [1.0,0.0,0.0,1.0],
+            click_pos: Default::default(),
+            radius: 0.0
         });
         let model = meshes::MODEL2;
         ctx.draw(model.start, model.len() as i32, 1);
@@ -58,37 +61,38 @@ impl EventHandler for Stage {
 
 fn main() {
     miniquad::start(conf::Conf::default(), |mut ctx| {
-        UserData::owning(Stage::new(&mut ctx), ctx)
+        UserData::owning(Game::new(&mut ctx), ctx)
     });
 }
 
 mod shader {
+    use glam::{Mat4, Vec2};
     use miniquad::*;
 
-    pub const VERTEX: &str = r#"#version 100
-    attribute vec2 pos;
-    uniform vec2 offset;
-    void main() {
-        gl_Position = vec4(pos * 0.5 + offset, 0, 1);
-    }"#;
-
-    pub const FRAGMENT: &str = r#"#version 100
-
-    void main() {
-        gl_FragColor = vec4(1,0,0,1);
-    }"#;
+    pub const VERTEX: &str = include_str!("shader/vertex.glsl");
+    pub const FRAGMENT: &str = include_str!("shader/fragment.glsl");
 
     pub fn meta() -> ShaderMeta {
         ShaderMeta {
             images: vec![],
             uniforms: UniformBlockLayout {
-                uniforms: vec![UniformDesc::new("offset", UniformType::Float2)],
+                uniforms: vec![
+                    UniformDesc::new("camera", UniformType::Mat4),
+                    UniformDesc::new("model", UniformType::Mat4),
+                    UniformDesc::new("color", UniformType::Float4),
+                    UniformDesc::new("clickPos", UniformType::Float2),
+                    UniformDesc::new("radius", UniformType::Float1)
+                ],
             },
         }
     }
 
     #[repr(C)]
     pub struct Uniforms {
-        pub offset: (f32, f32),
+        pub camera: Mat4,
+        pub model: Mat4,
+        pub color: [f32; 4],
+        pub click_pos: Vec2,
+        pub radius: f32
     }
 }
